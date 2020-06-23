382d69d793561f4495e06231ded593a626c3b9cc
btaskew <btaskew@hotmail.co.uk>
2020-06-23 07:46:22 +0100

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/ideas');
 });
 
 Route::get('/ideas', 'IdeasController@index')->name('home');
