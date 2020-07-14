ff1796cec1c3b984acb64b55ffab9922421a7af6
Ben Askew <btaskew@hotmail.co.uk>
2020-07-14 14:03:04 +0100

    Add vote recorded mail notification

--- app/Vote.php
+++ app/Vote.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use App\Notifications\VoteRecorded;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vote extends Model
 {
@@ -10,4 +12,21 @@ class Vote extends Model
      * @var string[]
      */
     protected $fillable = ['idea_id', 'user_id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function (Vote $vote) {
+            $vote->idea->creator->notify(new VoteRecorded($vote->idea));
+        });
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function idea(): BelongsTo
+    {
+        return $this->belongsTo(Idea::class);
+    }
 }
